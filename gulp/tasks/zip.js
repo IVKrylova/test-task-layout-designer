8a11fc97559361a8d31df5bdf966f0aa0d504a0d
IVKrylova <ivk.lis@yandex.ru>
2022-12-12 08:57:34 +0300

    fix: change path

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -10,6 +10,6 @@ export const zip = () => {
         message: 'Error: <%= error.message %>',
       })
     ))
-    .pipe(zipPlugin(`${app.path.rootFolder}`.zip))
+    .pipe(zipPlugin(`${app.path.rootFolder}.zip`))
     .pipe(app.gulp.dest('./'));
 }
